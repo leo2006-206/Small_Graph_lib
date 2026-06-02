6fcf474f78efdbe2542c849bfaac1c8936084045
leo2006-206 <ss121073121073@gmail.com>
2026-06-02 23:09:20 +0100

    setup basic testcase

--- Driver/temp.cpp
+++ Driver/temp.cpp
@@ -1,36 +1,24 @@
 import std;
 
+template<bool b_v>
+struct s{
+    int i{123};
+
+    // Trailing requires clause goes AFTER the parameters
+    int rt(int a, int b) requires (b_v == true) {
+        return i + a + b;
+    }
+
+    // Notice the parentheses around the negation!
+    int rt(int a) requires (b_v == false) {
+        return i + a;
+    }
+};
 
 int main(void){
+    s<true> mys;
+    std::println("{}", mys.rt(1, 2));
 
-	// std::vector v{1, 2, 4, 5};
-
-	// std::print("vector is {}\n", v);
-
-	// auto it = std::ranges::lower_bound(v, 3);
-
-	// auto it2 = v.emplace(it, 3);
-
-	// std::print("vector is {}\n", v);
-
-	// *it2 = 6;
-
-	// std::print("vector is {}\n", v);
-
-	// auto& ref = v.emplace_back(7);
-
-	// std::print("vector is {}\n", v);
-
-	// ref = 8;
-
-	// std::print("vector is {}\n", v);
-
-	std::set<int> s;
-
-	s.emplace(std::numeric_limits<int>::max());
-	s.emplace(std::numeric_limits<int>::min());
-
-	for(auto i : s){
-		std::cout<<(std::bitset<32>(i))<<"\t"<<i<<"\n";
-	}
+    s<false> mys2;
+    std::println("{}", mys2.rt(1));
 }
